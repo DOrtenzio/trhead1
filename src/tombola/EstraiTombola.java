8f084253c03b85cab21700cfce5dc37e415e9e3c
DOrtenzio <gutroch@gmail.com>
2024-09-20 19:00:25 +0200

    Versione 2 ma con ancora qualche ripetizione sul susseguirsi di due estrazioni, forse per mancata sincronizzazione dell'array?

--- src/tombola/EstraiTombola.java
+++ src/tombola/EstraiTombola.java
@@ -1,31 +1,31 @@
 package tombola;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class EstraiTombola extends Thread {
-    //VERSIONE DUE MA CON QUALCHE RIPETIZIONE
+    //VERSIONE DUE MA CON QUALCHE RIPETIZIONE SOPRATTUTTO SUL SUSSEGUIRSI
     String nomePersona;
-    boolean [] numeriEstratti=new boolean[90];
+
+    static ArrayList <Integer> numeriEstratti=new ArrayList<>();
 
     public void settaNomePersona(String nome) {
         nomePersona=nome;
     }
 
     public void aggNum(int i) {
-        numeriEstratti[i]=true;
+        numeriEstratti.add(i);
     }
 
-    public boolean contrNumEstratti(int num) {
-        return numeriEstratti[num];
+    public boolean contrNumEstratti(Integer num) {
+        return !numeriEstratti.contains(num);
     }
-
 
     public void estraiNumero() {
         int temp;
         do {
-            temp= (int) (Math.random()*90);
-            System.out.println(contrNumEstratti(temp));
-        }while(contrNumEstratti(temp));
+            temp= (int) ((Math.random()*90)+1);
+        }while(!contrNumEstratti(temp));
         System.out.println(nomePersona+" ha estratto: "+temp);
         aggNum(temp);
         try {
@@ -36,9 +36,9 @@ public class EstraiTombola extends Thread {
     }
 
     public void run() {
-        for (int i=0;i<90;i++){
+        do {
             estraiNumero();
-        }
+        } while (numeriEstratti.size() != 90);
     }
 
 }
--- src/tombola/Main.java
+++ src/tombola/Main.java
@@ -1,13 +1,13 @@
 package tombola;
 
 public class Main {
-    //VERSIONE DUE MA CON QUALCHE RIPETIZIONE
+    //VERSIONE DUE MA CON QUALCHE RIPETIZIONE SOPRATTUTTO SUL SUSSEGUIRSI
     public static void main(String[] args) {
         EstraiTombola p1=new EstraiTombola();
         p1.settaNomePersona("E1");
+        p1.start();
         EstraiTombola p2=new EstraiTombola();
         p2.settaNomePersona("E2");
-        p1.start();
         p2.start();
     }
 }

01150642b2dee0e27fc31470f130cb1f5947dd8d
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-09-20 19:02:53 +0200

    Update Main.java

--- src/tombola/Main.java
+++ src/tombola/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         EstraiTombola p1=new EstraiTombola();
         p1.settaNomePersona("E1");
-        p1.start();
         EstraiTombola p2=new EstraiTombola();
         p2.settaNomePersona("E2");
+        p1.start();
         p2.start();
     }
 }

5824178f69a4c64a474237687185563768046a10
dortenzio.21125 <dortenzio.diego.studente@itispaleocapa.it>
2024-09-20 11:29:36 +0200

    Correzione

--- src/tombola/EstraiTombola.java
+++ src/tombola/EstraiTombola.java
@@ -17,6 +17,14 @@ public class EstraiTombola extends Thread {
         numeriEstratti.add(i);
     }
 
+    public int leggiContatore() {
+        return cont;
+    }
+
+    public void settaContatore(int valore) {
+        cont=valore;
+    }
+
     public boolean contrNumEstratti(int num) {
         for (int i=0;i<cont;i++){
             if (numeriEstratti.get(i) == num)
@@ -27,12 +35,13 @@ public class EstraiTombola extends Thread {
 
     public void estraiNumero() {
         int temp;
+        int c=leggiContatore();
         do {
             temp= (int) (Math.random()*90);
         }while(!contrNumEstratti(temp));
         aggNum(temp);
-        cont+=1;
-        System.out.println(nomePersona+":"+temp+" num estratti: "+cont);
+        settaContatore(c+1);
+        System.out.println(nomePersona+":"+temp);
         try {
             TimeUnit.SECONDS.sleep(1);
         }catch(Exception e) {

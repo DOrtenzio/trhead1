2bfe6dcf2e27815fa6738d23888b7ab0cace8751
dortenzio.21125 <dortenzio.diego.studente@itispaleocapa.it>
2024-09-20 11:45:50 +0200

    Tombola

--- src/tombola/EstraiTombola.java
+++ src/tombola/EstraiTombola.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class EstraiTombola extends Thread {
-    int cont=0;
     String nomePersona;
 
-    ArrayList <Integer> numeriEstratti=new ArrayList<>();
+    static ArrayList <Integer> numeriEstratti=new ArrayList<>();
 
     public void settaNomePersona(String nome) {
         nomePersona=nome;
@@ -17,16 +16,9 @@ public class EstraiTombola extends Thread {
         numeriEstratti.add(i);
     }
 
-    public int leggiContatore() {
-        return cont;
-    }
-
-    public void settaContatore(int valore) {
-        cont=valore;
-    }
 
     public boolean contrNumEstratti(int num) {
-        for (int i=0;i<cont;i++){
+        for(int i=0;i<numeriEstratti.size();i++){
             if (numeriEstratti.get(i) == num)
                 return false;
         }
@@ -35,13 +27,11 @@ public class EstraiTombola extends Thread {
 
     public void estraiNumero() {
         int temp;
-        int c=leggiContatore();
         do {
             temp= (int) (Math.random()*90);
         }while(!contrNumEstratti(temp));
+        System.out.println(nomePersona+" ha estratto: "+temp);
         aggNum(temp);
-        settaContatore(c+1);
-        System.out.println(nomePersona+":"+temp);
         try {
             TimeUnit.SECONDS.sleep(1);
         }catch(Exception e) {
@@ -52,7 +42,7 @@ public class EstraiTombola extends Thread {
     public void run() {
         do {
             estraiNumero();
-        } while (cont != 90);
+        } while (numeriEstratti.size() != 90);
     }
 
 }

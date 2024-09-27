5d91dd3f14ff8ae5410a2a95c600090b965f4446
dortenzio.21125 <dortenzio.diego.studente@itispaleocapa.it>
2024-09-27 11:51:00 +0200

    Commentato tutto il codice

--- src/tombola/EstraiTombola.java
+++ src/tombola/EstraiTombola.java
@@ -5,31 +5,42 @@ import java.util.concurrent.TimeUnit;
 
 public class EstraiTombola extends Thread {
     //VERSIONE DUE
+
+    //Stringa identificativa dell'estrattore
     String nomePersona;
 
+    //Array in comune dove sono contenuti tutti i num da 1 a 90
     private static ArrayList <Integer> numeriDisponibili =new ArrayList<>();
 
+    //Inposto il nome dell'estrazione
     public void setEstrazione(String nome) {
         nomePersona=nome;
     }
 
+    //Inserisco nell'arraylist tutti i num da 1 a 90
     public void popolaArray(){
         for (int i=1;i<91;i++){ numeriDisponibili.add(i); }
     }
 
+    //Cancello il numero con l'indice estratto casualmente
     public void removNumero(int i) {
         numeriDisponibili.remove(i);
     }
 
+    //Estrazione di un numero
     public void estraiNumero() {
+        //Estraggo l'indice casuale
         int i= (int) (Math.random() * numeriDisponibili.size());
+        //Restituisco un informazione sulla codizione del tombolino o indico il numero estratto
         String s;
         if (numeriDisponibili.isEmpty())
             s="Numeri terminati.";
         else
-            s=nomePersona+" ha estratto: "+numeriDisponibili.get(i)+" size: "+numeriDisponibili.size();
+            s=nomePersona+" ha estratto: "+numeriDisponibili.get(i);
         System.out.println(s);
+        //Rimuovo il numero
         removNumero(i);
+        //Pausa così che il secondo estrattore abbia il tempo di estrarre il suo numero così che non capiti ch possano essere estratte 2 indici uguali
         try {
             TimeUnit.SECONDS.sleep(1);
         }catch(Exception e) {
--- src/tombola/TestTombola.java
+++ src/tombola/TestTombola.java
@@ -1,30 +1,28 @@
 package tombola;
 
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class TestTombola {
     //VERSIONE DUE
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
+
+        //Creo i due oggetti EstraiTombola
         EstraiTombola p1=new EstraiTombola();
         p1.setEstrazione("E1");
         EstraiTombola p2=new EstraiTombola();
         p2.setEstrazione("          E2");
+
+        //Popolo l'unico array
         p1.popolaArray();
-        p1.start();
+
+        //Inizio il metodo
+        p1.start();     //Prima Estrazione
         try {
             TimeUnit.SECONDS.sleep(1);
         }catch(Exception e) {
             System.out.println(e);
         }
-        p2.start();
+        p2.start();     //Seconda Estrazione
 
-        System.out.println("Inserire ENTER per uscire");
-        try {
-            in.nextLine();
-        }catch(Exception e) {
-            System.out.println(e);
-        }
     }
 }
